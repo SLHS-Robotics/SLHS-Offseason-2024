e465c4d27088fd5ba6df785a918e6573e776debd
joey <milausnic@gmail.com>
2024-03-19 15:21:33 -0400

    Add opmode run

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop.java
@@ -6,6 +6,12 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 @TeleOp
 public class teleop extends LinearOpMode {
     public void runOpMode() {
+        waitForStart();
 
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+            // Run
+        }
+        }
     }
 }
