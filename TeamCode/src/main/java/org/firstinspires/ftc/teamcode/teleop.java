908e3720e174e8f310011f25e311360ce733ef7d
0novo <tianxydin@gmail.com>
2024-03-22 15:47:36 -0400

    capitalization, minor formatting

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop.java
@@ -2,22 +2,20 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp
 public class teleop extends LinearOpMode {
-    public DcMotor turntablemotor;
-    public DcMotor armmotor;
-    public Servo armservo;
-    public Servo wristservo;
-    public Servo clawservo;
-
+    // Arm Motors and Servos
+    public DcMotor turntableM;
+    public DcMotor armM;
+    public Servo armS;
+    public Servo wristS;
+    public Servo clawS;
   
-    //Define Motors
+    // Wheel (Movement) Motors
     public DcMotor frontRightM;
     public DcMotor frontLeftM;
     public DcMotor backRightM;
@@ -48,30 +46,26 @@ public class teleop extends LinearOpMode {
         backRightM.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         backLeftM.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
+        turntableM = hardwareMap.get(DcMotor.class,"turntable");
+        armM = hardwareMap.get(DcMotor.class,"armmotor");
+        armS = hardwareMap.get(Servo.class,"armservo");
+        wristS = hardwareMap.get(Servo.class,"wrist");
+        clawS = hardwareMap.get(Servo.class,"claw");
 
-       turntablemotor = hardwareMap.get(DcMotor.class,"turntable");
-       armmotor= hardwareMap.get(DcMotor.class,"armmotor");
+        turntableM.setDirection(DcMotor.Direction.FORWARD);
+        armM.setDirection(DcMotor.Direction.FORWARD);
+        armS.setDirection(Servo.Direction.FORWARD);
+        wristS.setDirection(Servo.Direction.FORWARD);
+        clawS.setDirection(Servo.Direction.FORWARD);
 
-       armservo= hardwareMap.get(Servo.class,"armservo");
-       wristservo = hardwareMap.get(Servo.class,"wrist");
-       clawservo = hardwareMap.get(Servo.class,"claw");
+        turntableM.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        turntableM.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        armM.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        armM.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
-        turntablemotor.setDirection(DcMotor.Direction.FORWARD);
-        armmotor.setDirection(DcMotor.Direction.FORWARD);
-
-        armservo.setDirection(Servo.Direction.FORWARD);
-        wristservo.setDirection(Servo.Direction.FORWARD);
-        clawservo.setDirection(Servo.Direction.FORWARD);
-
-        turntablemotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        armmotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-
-        turntablemotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        armmotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
-        armservo.setPosition(0.0);
-        wristservo.setPosition(0.0);
-        clawservo.setPosition(0.0);
+        armS.setPosition(0.0);
+        wristS.setPosition(0.0);
+        clawS.setPosition(0.0);
 
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
